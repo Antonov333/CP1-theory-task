b5e8fe3e12cd8cad1694d1766a60604781308df4
Antonov333 <antonovsergei333@yahoo.com>
2023-03-31 16:27:31 +0300

    Stsrting solving the problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Билет 2\nЗадача: напишите программу, которая проверяет, является ли строка палиндромом.");
     }
 }
